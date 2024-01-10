467e4e30ea0bc3360e3961f7a629723a181b86d6
xiri <xiri@UbuntuDesktopSymphony.myguest.virtualbox.org>
2024-01-10 08:36:36 +0100

    Commit Twig EquipsJavi

--- src/Controller/EquipsController.php
+++ src/Controller/EquipsController.php
@@ -12,7 +12,7 @@ class EquipsController extends AbstractController
     array("codi" => "2", "nom" => "Equip Blau", "cicle" =>"DAW", "curs"=>"23/24", "membres" => array("Miquel","Javi","Jose","Angel")),
     array("codi" => "3", "nom" => "Equip Verd", "cicle" =>"DAW", "curs"=>"23/24", "membres" => array("Isma","Pablo","Hugo","Joan")),
     array("codi" => "4", "nom" => "Equip Groc", "cicle" =>"DAW", "curs"=>"23/24", "membres" => array("Kiko","Joselu","Jorge","Hector")));
-    #[Route('/equip/{codi}', name: 'dades_equip')]
+    #[Route('/equip/{codi?1}', name: 'dades_equips')]
     public function fitxa($codi)
     {
         $result = array_filter($this->equips,
@@ -22,18 +22,16 @@ class EquipsController extends AbstractController
         });
         if (count($result)>0)
         {
-            $resposta = "";
             $result = array_shift($result);
-            $resposta .= "<ul><li> Nom: " . $result["nom"] . "</li>" .
-            "<li> Cicle: " . $result["cicle"] . "</li>" .
-            "<li> Curs: " . $result["curs"] . "</li>" .
-            "<li> Membres: " . implode(', ',$result["membres"]) . "</li></ul>";
-            return new Response("<html><body>$resposta</body></html>");
+            //return new Response("<html><body>$resposta</body></html>");
+            return $this->render('equips/dades_equip.html.twig',
+        array('equip' => $result));
             
         }
         else
         {
-            return new Response("No s'ha trobat l'equip: $codi");
+            return $this->render('equips/dades_equip.html.twig',
+        array('equip' => NULL));
         }
         
     }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class IniciController extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('/inici', name: 'inici')]
-    public function inici()
+    #[Route('/', name: 'home')]
+    public function home()
     {
         //return new Response('Gestió d’equips del projecte de 2n de DAW');
-        return $this->render('inici/inici.html.twig');
+        return $this->render('base.html.twig');
     }
 }
 ?>
